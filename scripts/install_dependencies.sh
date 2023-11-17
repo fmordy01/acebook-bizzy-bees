ec6e966a27d3bd2b06ae5c5660979c0167cf42a7
Rob Rowe <gizmo.hacker585@4wrd.cc>
2023-11-17 16:30:14 +0000

    added commands to GitHub workflow rather than script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-cd /var/acebook
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 18
-npm install
-wget -o /etc/yum.repos.d/mongodb-org-5.0.repo https://repo.mongodb.org/yum/amazon/mongodb-org.repo
-sudo yum install -y mongodb-org
-sudo systemctl start --now mongod
+# cd /var/acebook
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install 18
+# npm install
+# wget -o /etc/yum.repos.d/mongodb-org-5.0.repo https://repo.mongodb.org/yum/amazon/mongodb-org.repo
+# sudo yum install -y mongodb-org
+# sudo systemctl start --now mongod
+echo "Dependencies installed by GitHub workflow?"
