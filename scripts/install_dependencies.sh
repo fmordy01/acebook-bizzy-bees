7057bbd81e541bce8c7ec60358f957f41c99388b
Rob Rowe <gizmo.hacker585@4wrd.cc>
2023-11-17 15:37:32 +0000

    add installation of nvm and mongodb to dependencies installation script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd /var/acebook
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+. ~/.nvm/nvm.sh
 nvm install 18
 npm install
+wget -o /etc/yum.repos.d/mongodb-org-7.0.repo https://repo.mongodb.org/yum/amazon/mongodb-org.repo
+sudo yum install -y mongodb-org
 sudo systemctl start --now mongod
