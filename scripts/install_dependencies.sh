dec8515aa1fcbd0bd80e566ca7d75f5820be0c2d
Rob Rowe <gizmo.hacker585@4wrd.cc>
2023-11-17 15:47:16 +0000

    change GitHub Workflows ymls and MongoDB installation script, -> Ubuntu20.04

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 18
 npm install
-wget -o /etc/yum.repos.d/mongodb-org-7.0.repo https://repo.mongodb.org/yum/amazon/mongodb-org.repo
-sudo yum install -y mongodb-org
+sudo apt-get install gnupg curl
+curl -fsSL https://pgp.mongodb.com/server-5.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-5.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 sudo systemctl start --now mongod
